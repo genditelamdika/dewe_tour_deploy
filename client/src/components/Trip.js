6dc783afa1be76abfbad2b48fcb3fab91dfc4350
genditelamdika <gendydika@gmail.com>
2023-06-11 20:24:22 -0600

    detaillmm

--- client/src/components/Trip.js
+++ client/src/components/Trip.js
@@ -201,7 +201,7 @@ function Trip() {
                   </Card.Body>
             </Link>
                   <Card.Body>
-                    <button onClick={handleDelete} type="buton" className="shadow  btn btn-danger fw-bold  " style={{width:"70px"}} name={item.id} value={item.id}>Delete</button>
+                    <button onClick={handleDelete} type="buton" className="shadow  btn btn-danger fw-bold  " style={{width:"90px"}} name={item.id} value={item.id}>Delete</button>
                   </Card.Body>
                 </Card>
               </Col>
